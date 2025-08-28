52f28c6942c58466a64686b5aa3e5108b3a0ca22
PintilieGeani <pintilie.geany@yahoo.it>
2025-08-28 17:34:37 +0200

    feat: milestone-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect} from "react"
+import { useState, useEffect , useMemo} from "react"
 
 function App() {
   const apiUrl = "http://localhost:3333/politicians"
@@ -18,12 +18,39 @@ function App() {
     fetchData()
   }, [])
 
+  const[search, setSearch] = useState("")
+  const filtrati = useMemo(() =>{
+    return politici.filter((pol) => pol.name.toLowerCase().includes(search.toLowerCase()) || pol.biography.toLowerCase().includes(search.toLowerCase()))
+  },[search, politici])
+
+  console.log(filtrati)
+
   return (
     <>
     <div className="impaginazione">
+      <div className="ricerca">
+        <h3>Cerca il tuo Politico</h3>
+        <input 
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+         />
+      </div>
       <div className="card-container">
-        {
-          politici.map((politico, id) => (
+        {filtrati.length > 0 ? 
+          filtrati.map((politico, id) => (
+            <div key={id} className="card">
+              <div className="card-text">
+              <h3>{politico.name}</h3>
+              <p><strong>Posizione:</strong>{politico.position}</p>
+              <p><strong>Breve Biografia:</strong>{politico.biography}</p>
+              </div>
+              <div className="card-img">
+                <img src={politico.image} alt={politico.name} />
+              </div>
+            </div>
+          ))
+          : politici.map((politico, id) => (
             <div key={id} className="card">
               <div className="card-text">
               <h3>{politico.name}</h3>
